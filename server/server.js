ed9b1ffc032abbc9f8e14c1e1ead3cd73403f73c
carloserna12 <carlos.mauricio.serna@gmail.com>
2023-05-13 10:07:38 -0500

    post firebase configuration server

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "carlos.mauricio.serna@gmail.com",
+    pass: "rwftcmivubjqebra"
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "carlos.mauricio.serna@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
