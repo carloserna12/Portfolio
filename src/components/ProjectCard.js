307726ee9942b708668f0f357c96e21f3da9e363
carloserna12 <carlos.mauricio.serna@gmail.com>
2023-05-15 22:14:21 -0500

    some icons add, add filter

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,22 +2,48 @@ import { Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngular, faBootstrap, faCss3, faGithub, faHtml5, faJava, faJs, faNode, faNodeJs, faPython, faReact } from "@fortawesome/free-brands-svg-icons";
 import { faDatabase, faEarthAmerica, faLeaf, faServer } from "@fortawesome/free-solid-svg-icons";
+import java from "../assets/img/java.svg";
+import js from "../assets/img/javascript.svg";
+import nodeJs from "../assets/img/nodejs.svg";
+import angular from "../assets/img/angular.svg";
+import aws from "../assets/img/aws.svg";
+import cloud from "../assets/img/cloud.svg";
+import csharp from "../assets/img/csharp.svg";
+import django from "../assets/img/django.svg";
+import docker from "../assets/img/docker.svg";
+import firebase from "../assets/img/firebase.svg";
+import mongo from "../assets/img/mongo.svg";
+import mysql from "../assets/img/mysql.svg";
+import postgresql from "../assets/img/postgresql.svg";
+import python from "../assets/img/python.svg";
+import reacticon from "../assets/img/react.svg";
+import spring from "../assets/img/spring.svg";
+import sqlserver from "../assets/img/sqlserver.svg";
+import typescript from "../assets/img/typescript.svg";
+import unity from "../assets/img/unity.svg";
+import vue from "../assets/img/vue.svg";
 export const ProjectCard = ({ title, description, imgUrl,tecn,urlGit,urlWeb,status }) => {
   const iconos = {
-    "Java": faJava,
-    "Python": faPython,
-    "JS": faJs,
-    "SQL":faDatabase,
-    "Node":faNodeJs,
-    "Angular":faAngular,
-    "Boostrap":faBootstrap,
-    "CSS":faCss3,
-    "HTML":faHtml5,
-    "React":faReact,
-    "Api-rest":faServer,
-    "Springboot":faLeaf,
-    "Firebase":faDatabase,
-    // Agrega más íconos según sea necesario
+    "Java": java,
+    "Python": python,
+    "JavaScript": js,
+    "MySql":mysql,
+    "NodeJs":nodeJs,
+    "Angular":angular,
+    "React":reacticon,
+    "Spring":spring,
+    "Firebase":firebase,
+    "Vue":vue,
+    "Aws":aws,
+    "Cloud":cloud,
+    "CSharp":csharp,
+    "Django":django,
+    "Docker":docker,
+    "Mongo":mongo,
+    "PostgreSql":postgresql,
+    "SqlServer":sqlserver,
+    "TypeScript":typescript,
+    "Unity":unity,
   };
   return (
     <Col size={12} sm={6} md={4}>
@@ -33,7 +59,7 @@ export const ProjectCard = ({ title, description, imgUrl,tecn,urlGit,urlWeb,stat
       <div className="tecn-container">
       {tecn.map((item, index) => (
           <span key={index} className="tech-container">
-            <span className="tech"> <FontAwesomeIcon icon={iconos[item]} className="tech-icon" /> {item}</span>
+            <span className="tech"> <img src={iconos[item]} alt={item} className="tech-icon" /> {item}</span>
           </span>
         ))}
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,10 +4,30 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import projImg_1 from "../assets/img/project-img-1.png";
-import "animate.css";
+import projImg_1 from "../assets/img/project-img-1.png"
+;import "animate.css";
 import TrackVisibility from "react-on-screen";
-
+import React, { useState } from 'react';
+import java from "../assets/img/java.svg";
+import js from "../assets/img/javascript.svg";
+import nodeJs from "../assets/img/nodejs.svg";
+import angular from "../assets/img/angular.svg";
+import aws from "../assets/img/aws.svg";
+import cloud from "../assets/img/cloud.svg";
+import csharp from "../assets/img/csharp.svg";
+import django from "../assets/img/django.svg";
+import docker from "../assets/img/docker.svg";
+import firebase from "../assets/img/firebase.svg";
+import mongo from "../assets/img/mongo.svg";
+import mysql from "../assets/img/mysql.svg";
+import postgresql from "../assets/img/postgresql.svg";
+import python from "../assets/img/python.svg";
+import reacticon from "../assets/img/react.svg";
+import spring from "../assets/img/spring.svg";
+import sqlserver from "../assets/img/sqlserver.svg";
+import typescript from "../assets/img/typescript.svg";
+import unity from "../assets/img/unity.svg";
+import vue from "../assets/img/vue.svg";
 export const Projects = () => {
   const projects = [
     {
@@ -15,7 +35,7 @@ export const Projects = () => {
       description:
         "Web basica personal para dentista con Node, Firebase, boostrap y Javascript ",
       imgUrl: projImg_1,
-      tecn: ["Node", "Firebase", "JS", "Boostrap"],
+      tecn: ["NodeJs", "Firebase", "JavaScript"],
       urlGit: "https://github.com/carloserna12/Web-Dentista",
       urlWeb: "https://dralejandrorobles.netlify.app",
       status: "Deploy",
@@ -24,7 +44,7 @@ export const Projects = () => {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
-      tecn: ["Java", "Python"],
+      tecn: ["Aws", "Python","React","Django"],
       status: "InProgress",
       status: "DeployNot",
     },
@@ -32,21 +52,58 @@ export const Projects = () => {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
-      tecn: ["Java", "Python"],
+      tecn: ["SqlServer", "Python","Vue"],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
-      tecn: ["Java", "Python", "JS", "SQL"],
+      tecn: ["Mongo", "CSharp", "Docker","Unity"],
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
-      tecn: ["JS", "Springboot", "SQL"],
+      tecn: ["JavaScript", "Spring", "PostgreSql","Java"],
     },
   ];
+
+  const [checkedItems, setCheckedItems] = useState([]);
+
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setCheckedItems([...checkedItems, value]);
+    } else {
+      setCheckedItems(checkedItems.filter((item) => item !== value));
+    }
+  };
+
+
+  const technologies = ['Java', 'Python', 'JavaScript', 'MySql', 'NodeJs','Angular','React','Spring','Firebase','Vue','Aws','Cloud','CSharp','Django','Docker','Mongo','PostgreSql','SqlServer','TypeScript','Unity'];
+  
+  const iconos = {
+    "Java": java,
+    "Python": python,
+    "JavaScript": js,
+    "MySql":mysql,
+    "NodeJs":nodeJs,
+    "Angular":angular,
+    "React":reacticon,
+    "Spring":spring,
+    "Firebase":firebase,
+    "Vue":vue,
+    "Aws":aws,
+    "Cloud":cloud,
+    "CSharp":csharp,
+    "Django":django,
+    "Docker":docker,
+    "Mongo":mongo,
+    "PostgreSql":postgresql,
+    "SqlServer":sqlserver,
+    "TypeScript":typescript,
+    "Unity":unity,
+  };
   return (
     <section className="project" id="project">
       <Container>
@@ -67,7 +124,7 @@ export const Projects = () => {
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav
                       variant="pills"
-                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      className="nav-pills mb-4 justify-content-center align-items-center"
                       id="pills-tab"
                     >
                       <Nav.Item>
@@ -78,10 +135,31 @@ export const Projects = () => {
                       </Nav.Item>
                       <Nav.Item>
                         <Nav.Link eventKey="third">
-                          Ready for Deployment
+                        Rdy to deploy
                         </Nav.Link>
                       </Nav.Item>
                     </Nav>
+
+
+                    <div className="checkbox-list">
+                      {technologies.map((technology) => (
+                        <div key={technology} className="checkbox-item">
+                          <label className="checkbox-label">
+                            <input
+                              type="checkbox"
+                              value={technology}
+                              checked={checkedItems.includes(technology)}
+                              onChange={handleCheckboxChange}
+                            />
+                            <img src={iconos[technology]} alt={technology} className="tech-icon-filter" />
+                            {technology}
+                          </label>
+                        </div>
+                      ))}
+                    </div>
+
+
+
                     <Tab.Content
                       id="slideInUp"
                       className={
@@ -90,16 +168,25 @@ export const Projects = () => {
                     >
                       <Tab.Pane eventKey="first">
                         <Row>
-                          {projects.map((project, index) => (
-                            <ProjectCard key={index} {...project} />
-                          ))}
+                          {projects
+                            .filter((project) =>
+                              checkedItems.length === 0 ||
+                              checkedItems.every((item) => project.tecn.includes(item))
+                            )
+                            .map((project, index) => (
+                              <ProjectCard key={index} {...project} />
+                            ))}
                         </Row>
                       </Tab.Pane>
 
                       <Tab.Pane eventKey="second">
                         <Row>
                           {projects
-                            .filter((project) => project.status === "Deploy")
+                            .filter(
+                              (project) =>
+                                project.status === "Deploy" &&
+                                checkedItems.every((item) => project.tecn.includes(item))
+                            )
                             .map((project, index) => (
                               <ProjectCard key={index} {...project} />
                             ))}
@@ -109,12 +196,19 @@ export const Projects = () => {
                       <Tab.Pane eventKey="third">
                         <Row>
                           {projects
-                            .filter((project) => project.status === "DeployNot")
+                            .filter(
+                              (project) =>
+                                project.status === "DeployNot" &&
+                                checkedItems.every((item) => project.tecn.includes(item))
+                            )
                             .map((project, index) => (
                               <ProjectCard key={index} {...project} />
                             ))}
                         </Row>
                       </Tab.Pane>
+
+                      
+                      
                     </Tab.Content>
                   </Tab.Container>
                 </div>
